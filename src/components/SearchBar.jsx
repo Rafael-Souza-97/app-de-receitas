89dace7a8693fa3d7d58994f3aeafae8f68d650f
Caio <caiusito@gmail.com>
2022-09-26 16:09:29 -0300

    iniciando requisito 11

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,21 +7,18 @@ function SearchBar() {
         type="radio"
         data-testid="ingredient-search-radio"
         value="nome-do-ingrediente"
-        // placeholder="Ingrediente"
       />
       Ingrediente
       <input
         type="radio"
         data-testid="name-search-radio"
         value="nome-da-receita"
-        // placeholder="nome da receita"
       />
       Nome da receita
       <input
         type="radio"
         data-testid="first-letter-search-radio"
         value="primeira-letra"
-        // placeholder="Começa com..."
       />
       Começa com ...
       <button
