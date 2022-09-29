761808243432950be4d5a543134f33ddc635f457
Rafael Souza <souza_rafael.97@outlook.com>
2022-09-29 14:27:48 -0300

    Email comentado para não gerar bugs no header/footer

--- src/components/ProfileButtons.jsx
+++ src/components/ProfileButtons.jsx
@@ -1,47 +1,53 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { getEmailFromLocalStorage } from '../services/localStorage';
+// import { getEmailFromLocalStorage } from '../services/localStorage';
 import '../styles/Profile.css';
 
 function ProfileButtons() {
-  const userEmail = getEmailFromLocalStorage('user');
+  // const userEmail = getEmailFromLocalStorage('user');
 
   return (
     <div className="section-container">
+      {/* <div>
+        <h3 data-testid="profile-email" className="email">
+          {
+            userEmail.email
+          }
+        </h3>
+      </div> */}
+
       <div>
-        <h3 data-testid="profile-email" className="email">{ userEmail.email }</h3>
+        <Link to="/done-recipes">
+          <button
+            type="button"
+            data-testid="profile-done-btn"
+            className="btn btn-secondary btn-lg btn-block"
+          >
+            Done Recipes
+          </button>
+        </Link>
+
+        <Link to="/favorite-recipes">
+          <button
+            type="button"
+            data-testid="profile-favorite-btn"
+            className="btn btn-secondary btn-lg btn-block"
+          >
+            Favorite Recipes
+          </button>
+        </Link>
+
+        <Link to="/">
+          <button
+            type="button"
+            data-testid="profile-logout-btn"
+            className="btn btn-lg btn-block btn-outline-danger"
+            onClick={ () => localStorage.clear() }
+          >
+            Logout
+          </button>
+        </Link>
       </div>
-
-      <Link to="/done-recipes">
-        <button
-          type="button"
-          data-testid="profile-done-btn"
-          className="btn btn-secondary btn-lg btn-block"
-        >
-          Done Recipes
-        </button>
-      </Link>
-
-      <Link to="/favorite-recipes">
-        <button
-          type="button"
-          data-testid="profile-favorite-btn"
-          className="btn btn-secondary btn-lg btn-block"
-        >
-          Favorite Recipes
-        </button>
-      </Link>
-
-      <Link to="/">
-        <button
-          type="button"
-          data-testid="profile-logout-btn"
-          className="btn btn-lg btn-block btn-outline-danger"
-          onClick={ () => localStorage.clear() }
-        >
-          Logout
-        </button>
-      </Link>
     </div>
   );
 }
