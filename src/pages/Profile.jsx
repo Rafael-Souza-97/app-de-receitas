b514b27709a66374ab77429bf9411e4d7177176d
Rafael Souza <souza_rafael.97@outlook.com>
2022-09-29 11:21:08 -0300

    Resolve erros no Header

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,51 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import Footer from '../components/Footer';
 import Header from '../components/Header';
-import { getEmailFromLocalStorage } from '../services/localStorage';
-import '../styles/Profile.css';
+import ProfileButtons from '../components/ProfileButtons';
+import Footer from '../components/Footer';
 
 function Profile() {
-  const userEmail = getEmailFromLocalStorage('user').email;
   return (
     <div className="container-fluid">
       <Header title="Profile" />
-
-      <h2 data-testid="profile-email" className="email">{ userEmail }</h2>
-
-      <section className="section-container">
-        <Link to="/done-recipes">
-          <button
-            type="button"
-            data-testid="profile-done-btn"
-            className="btn btn-secondary btn-lg btn-block"
-          >
-            Done Recipes
-          </button>
-        </Link>
-
-        <Link to="/favorite-recipes">
-          <button
-            type="button"
-            data-testid="profile-favorite-btn"
-            className="btn btn-secondary btn-lg btn-block"
-          >
-            Favorite Recipes
-          </button>
-        </Link>
-
-        <Link to="/">
-          <button
-            type="button"
-            data-testid="profile-logout-btn"
-            className="btn btn-lg btn-block btn-outline-danger"
-            onClick={ () => localStorage.clear() }
-          >
-            Logout
-          </button>
-        </Link>
-      </section>
-
+      <ProfileButtons />
       <Footer />
     </div>
   );
--- src/services/localStorage.js
+++ src/services/localStorage.js
@@ -7,6 +7,9 @@ export const addDrinksTokenLocalStorage = (token) => localStorage
 export const addEmailLocalStorage = (userEmail) => localStorage
   .setItem('user', JSON.stringify({ email: userEmail }));
 
-export const getEmailFromLocalStorage = (userEmail) => (
-  JSON.parse(localStorage.getItem(userEmail))
-);
+export const getEmailFromLocalStorage = (userEmail) => {
+  if (localStorage.getItem(userEmail) === null) {
+    return localStorage.setItem(userEmail, JSON.stringify({}));
+  }
+  return JSON.parse(localStorage.getItem(userEmail));
+};
